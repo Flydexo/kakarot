8d15769c413e1f1c5939e6d53ae43124e543a0cd
Flydexo <flydexo@gmail.com>
2023-01-19 23:31:05 +0100

    clean

--- tests/fixtures/6_kethaa.py
+++ tests/fixtures/6_kethaa.py
@@ -1,11 +1,4 @@
-import os
-from random import randrange
-from typing import Tuple
-
 import pytest_asyncio
-import web3
-from eth_account.account import Account, SignedMessage
-from eth_keys import keys
 from starkware.starknet.testing.contract import StarknetContract
 from starkware.starknet.testing.starknet import Starknet
 
@@ -41,18 +34,6 @@ async def default_tx() -> dict:
         "chainId": 1263227476,
         "maxFeePerGas": 1000,
         "maxPriorityFeePerGas": 667667,
-        "gas": 999999999,
-        "to": bytes.fromhex("95222290dd7278aa3ddd389cc1e1d165cc4bafe5"),
-        "value": 10000000000000000,
-        "data": b"",
-    }
-
-
-@pytest_asyncio.fixture(scope="package")
-async def legacy_tx() -> dict:
-    return {
-        "nonce": 1,
-        "gasPrice": 1000,
         "gas": 999999999,
         "to": bytes.fromhex("95222290dd7278aa3ddd389cc1e1d165cc4bafe5"),
         "value": 10000000000000000,
